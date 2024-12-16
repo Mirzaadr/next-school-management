55599a2921b3992ca5e2d837fc9149ce6d56b170
Mirzaadr <mirzaadr34@yahoo.com>
2024-12-17 03:03:35 +0700

    update config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "standalone",
+  env: {
+    PORT: process.env.PORT || 3000,
+  },
+
 };
 
 export default nextConfig;
