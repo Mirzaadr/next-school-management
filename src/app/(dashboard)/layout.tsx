9a02b21fef8a218f954e56ee7f9c6023e60859ba
Mirzaadr <mirzaadr34@yahoo.com>
2024-12-17 15:26:45 +0700

    create navbar

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,4 +1,5 @@
 import Menu from "@/components/Menu";
+import Navbar from "@/components/Navbar";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
@@ -20,6 +21,7 @@ export default function DashboardLayout({
       </div>
       {/* RIGHT */}
       <div className="w-full bg-[#F7F8FA] overflow-scroll">
+        <Navbar />
         {children}
       </div>
     </div>
